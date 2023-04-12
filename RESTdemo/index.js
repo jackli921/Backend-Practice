92a2beb80665a9d753113b51675c030b57c3dd68
Jack <li.jack0707@gmail.com>
2023-04-12 15:59:59 -0400

    feat: add edit comment functionality

--- RESTdemo/index.js
+++ RESTdemo/index.js
@@ -1,31 +1,36 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override')
 
 app.use(express.urlencoded({ extended: true })) //running some code on every single request, doesn't matter the request or path
 app.use(express.json())
-
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'ejs')
 
 
 const comments = [
     {
-        username:"Tod",
-        comment: "lol, that's so funny"
+        username:"Cat",
+        comment: "Meow meow meow",
+        id:uuidv4()
     },
     {
         username:"Dog",
-        comment: "troll!!"
+        comment: "WOOF WOOF WOOF",
+        id:uuidv4()
     },
     {
-        username:"Ted",
-        comment: "plz delete your account, Todd"
+        username:"Chicken",
+        comment: "cock-a-doodle-doo",
+        id:uuidv4()
     },
     {
-        username:"Ken",
-        comment: "Wow that's so cute"
+        username:"Cow",
+        comment: "Moooooo",
+        id:uuidv4()
     }
 ]
 // view all comments
@@ -43,19 +48,31 @@ app.get("/comments/new", (req , res)=>{
 // where the form submits and extract the data from the form
 app.post("/comments", (req, res)=>{
     const {username, comment} = req.body
-    comments.push({username, comment})
+    comments.push({username, comment, id: uuidv4()})
     res.redirect('/comments')
 })
 
-
-app.get('/tacos', (req, res)=>{
-    res.send("GET /tacos responses")
+// show individual comment
+app.get('/comments/:id', (req,res)=>{
+    const {id} = req.params
+    const comment = comments.find(post => post.id === id )
+    res.render('comments/show', { comment })
 })
 
-app.post('/tacos', (req, res)=>{
-    const {meat , qty} = (req.body)
-    console.log(req.body)
-    res.send(`OK, here are your ${qty} ${meat} tacos`)
+// finds comment with specified id and pass it ot view
+app.get('/comments/:id/edit', (req, res)=>{
+    const {id} = req.params
+    const comment = comments.find(comment => comment.id === id)
+    res.render('comments/edit', {comment})
+})
+
+// updating comments
+app.patch('/comments/:id', (req, res)=>{
+    const {id} = req.params
+    const newComment = req.body.comment
+    const foundComment = comments.find(post => post.id === id)
+    foundComment.comment = newComment
+    res.redirect("/comments")
 })
 
 app.listen(3000, ()=>{
