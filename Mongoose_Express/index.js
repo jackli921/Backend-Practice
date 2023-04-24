45df36d93ff6845ed4717bc28f75acac5e79a778
Jack <li.jack0707@gmail.com>
2023-04-24 16:30:15 -0400

    chore:added get route to all products

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -17,8 +17,10 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/dog', (req,res)=>{
-    res.send("WOOF!")
+app.get('/products', async (req,res)=>{
+    const products = await Product.find({})
+    console.log(products)
+    res.send("All products will be here ")
 })
 
 
