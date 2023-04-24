58b706969b57b2d4a01c1c1740418b872fb9b821
Jack <li.jack0707@gmail.com>
2023-04-24 16:16:31 -0400

    chore: added product schema

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -2,9 +2,10 @@ const express = require("express")
 const app = express();
 const path = require('path')
 const mongoose = require('mongoose')
+const Product = require('./models/product')
 
 
-mongoose.connect('mongodb://localhost:27017/shopApp')
+mongoose.connect('mongodb://localhost:27017/farmStand')
     .then(()=>{
         console.log("Mongo Connection OPEN!!!")
     })
---
+++ Mongoose_Express/models/product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true,
+        min: 0
+    },
+    category:{
+        type: String, 
+        enum: ["fruit", "veggie", "dairy"]
+    }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
