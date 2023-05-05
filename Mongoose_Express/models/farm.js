2cb8bb9ff728f27348d42a0af9e9495774c2194c
Jack <li.jack0707@gmail.com>
2023-05-05 13:50:45 -0400

    add delete all associated product with a farm when farm is deleted

--- Mongoose_Express/models/farm.js
+++ Mongoose_Express/models/farm.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const Product = require('./product')
 
 const farmSchema = Schema({
     name:{
@@ -21,6 +22,15 @@ const farmSchema = Schema({
             ref: 'Product'
         }
     ]
+})
+
+
+farmSchema.post('findOneAndDelete', async function(farm){
+
+    if(farm.products.length){
+        const res = await Product.deleteMany({_id: {$in: farm.products}})
+        console.log(res)
+    }
 })
 
 const Farm = mongoose.model('Farm', farmSchema)
