7362fde7e8a6b1f8d4b1b48ec658cdc6f888174f
Jack <li.jack0707@gmail.com>
2023-05-05 08:42:02 -0400

    chore: update frontend and handlers

---
+++ MERN-Stack-Tutorial/MERN-Stack-Tutorial-lesson-2/frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className="pages">
+        <Routes>
+          <Route 
+            path='/'
+            element={<Home />}
+          />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
