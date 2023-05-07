eea2f637bb1aa7921113cd0b47e692548b660a1c
Jack <li.jack0707@gmail.com>
2023-05-07 14:37:52 -0400

    chore: add contextProvider and reducer hook

--- MERN-Stack-Tutorial/MERN-Stack-Tutorial-lesson-2/frontend/src/App.tsx
+++ MERN-Stack-Tutorial/MERN-Stack-Tutorial-lesson-2/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
+
 function App() {
 
   return (
--- MERN-Stack-Tutorial/MERN-Stack-Tutorial-lesson-2/frontend/src/main.tsx
+++ MERN-Stack-Tutorial/MERN-Stack-Tutorial-lesson-2/frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
+import { WorkoutsContextProvider } from './context/WorkoutContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <WorkoutsContextProvider>
+      <App />
+    </WorkoutsContextProvider>
   </React.StrictMode>,
 )
