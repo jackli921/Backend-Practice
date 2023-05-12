3dc21f98e3776d7efb3ac1ee17c8e3bf64c6f1f2
Jack <li.jack0707@gmail.com>
2023-05-12 08:23:31 -0400

    practice: add cookie parser and retriving cookies

--- S47-Express-Router-Cookies/index.js
+++ S47-Express-Router-Cookies/index.js
@@ -3,11 +3,25 @@ const app = express()
 const ramenRoutes = require('./Routes/ramen')
 const tokyoRoutes = require('./Routes/tokyoRoutes')
 const adminRoutes = require('./Routes/admin')
+const cookieParser = require('cookie-parser')
 
+app.use(cookieParser())
 app.use("/fukuoka/ramen", ramenRoutes);
 app.use("/tokyo/cafe", tokyoRoutes )
 app.use("/admin", adminRoutes)
 
+
+app.get('/greet', (req, res)=>{
+    const {name, me, I} = req.cookies
+    res.send(`Hey There, ${name} ${me} ${I}`)
+})
+
+app.get('/setname', (req, res)=>{
+    res.cookie('name', 'Jake')
+    res.cookie('me', 'Hi')
+    res.cookie('I', 'Me')
+    res.send("Gave you a cookie")
+})
 
 app.listen(3030, (req,res)=>{
     console.log('listening on port 3030')
