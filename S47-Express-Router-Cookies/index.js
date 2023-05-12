76477523a6662a1625b136b3ff64d17fe4aec87c
Jack <li.jack0707@gmail.com>
2023-05-12 08:33:31 -0400

    practice: signed cookies through cookieParser secret

--- S47-Express-Router-Cookies/index.js
+++ S47-Express-Router-Cookies/index.js
@@ -5,7 +5,7 @@ const tokyoRoutes = require('./Routes/tokyoRoutes')
 const adminRoutes = require('./Routes/admin')
 const cookieParser = require('cookie-parser')
 
-app.use(cookieParser())
+app.use(cookieParser('thisismysecret'))
 app.use("/fukuoka/ramen", ramenRoutes);
 app.use("/tokyo/cafe", tokyoRoutes )
 app.use("/admin", adminRoutes)
@@ -21,6 +21,17 @@ app.get('/setname', (req, res)=>{
     res.cookie('me', 'Hi')
     res.cookie('I', 'Me')
     res.send("Gave you a cookie")
+})
+
+app.get('/getsignedcookies', (req, res)=>{
+    res.cookie("gender", "male", { signed: true });
+    res.send("Signed it")
+})
+
+app.get('/verify', (req,res)=>{
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
 })
 
 app.listen(3030, (req,res)=>{
